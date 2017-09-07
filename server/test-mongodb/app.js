fe466cc1bf43e4f5fc4f3e9e2d0564e74cac7a25
System Administrator <root@Vinayaks-MacBook-Air.local>
2017-09-06 20:06:20 -0400

    Fixing app.js error

--- server/test-mongodb/app.js
+++ server/test-mongodb/app.js
@@ -67,5 +67,5 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
   var password = process.env.MLAB_PASSWORD_WEBDEV;
   connectionString = 'mongodb://' + username + ':' + password;
-  connectionString += 'ds127564.mlab.com:27564/heroku_fjfsrc1g'; // use yours
+  connectionString += '@ds127564.mlab.com:27564/heroku_fjfsrc1g'; // use yours
 }
