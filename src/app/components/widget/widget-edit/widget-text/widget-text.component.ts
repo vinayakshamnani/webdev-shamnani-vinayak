d942fcd6cae37af7a03ead1cdbfb64687198a243
System Administrator <root@Vinayaks-MacBook-Air.local>
2017-11-06 22:12:32 -0500

    Add text widget

--- src/app/components/widget/widget-edit/widget-text/widget-text.component.ts
+++ src/app/components/widget/widget-edit/widget-text/widget-text.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {WidgetService} from '../../../../services/widget.service.client';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-widget-text',
@@ -6,10 +9,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./widget-text.component.css']
 })
 export class WidgetTextComponent implements OnInit {
+  widgetId: string;
+  widget: any;
+  widgetName: string;
+  widgetText: string;
+  rows: number;
+  placeholder: string;
+  formatted: boolean;
 
-  constructor() { }
+  constructor(private widgetService: WidgetService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe((params: any) => {this.widgetId = params['wgid'];}
+    );
+    this.widgetService.findWidgetById(this.widgetId)
+      .subscribe(
+        (widget: any) => {
+          this.widget = widget;
+          this.widgetName = this.widget['name'];
+          this.widgetText = this.widget['text'];
+          this.rows = this.widget['rows'];
+          this.placeholder = this.widget['placeholder'];
+          this.formatted = this.widget['formatted'];
+        }
+      );
   }
+
+  update(){
+    this.widget['name'] = this.widgetName ;
+    this.widget['text'] = this.widgetText;
+    this.widget['rows'] = this.rows;
+    this.widget['placeholder'] = this.placeholder;
+    this.widget['formatted'] = this.formatted;
+    console.log('Widget at 44 is ' + JSON.stringify(this.widget));
+    this.widgetService.updateWidget(this.widgetId, this.widget)
+      .subscribe(
+        (widget: any) => {
+          this.router.navigate(['../'],{relativeTo: this.activatedRoute});
+        }
+      );
+       }
+  delete() {
+      this.widgetService.deleteWidget(this.widgetId).subscribe(
+            (res: any) => {
+              this.router.navigate(['../'],{relativeTo:this.activatedRoute});
+            }
+        );
+     }
 
 }
